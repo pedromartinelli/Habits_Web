7f84908d686156526ae7a197d1ea83594cc8d050
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-01-25 09:07:33 -0300

    criado os checkboxes da criação de hábito e dos hábitos completados do dia

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,4 +1,8 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
+
 import { Check } from "phosphor-react";
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
 export function NewHabitForm() {
   return (
@@ -19,10 +23,35 @@ export function NewHabitForm() {
         Qual a recorrência?
       </label>
 
+
+
+      <div className='mt-3 flex flex-col gap-2'>
+        {
+          availableWeekDays.map(weekDay => {
+            return (
+              <Checkbox.Root key={weekDay} className='flex items-center gap-3 group'>
+                <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                  <Checkbox.Indicator>
+                    <Check size={20} className='rounded-lg text-white' />
+                  </Checkbox.Indicator>
+                </div>
+
+                <span className='text-white leading-tight'>
+                  {weekDay}
+                </span>
+              </Checkbox.Root>
+            );
+          })
+        }
+
+      </div>
+
+
+
       <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
         <Check size={20} weight='bold' />
         Confirmar
       </button>
-    </form>
+    </form >
   );
 }
